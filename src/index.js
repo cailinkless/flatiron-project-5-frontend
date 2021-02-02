d2ef290b24a81182d3349721481ea54a10c4781c
cailinkless <cailin.nysff@gmail.com>
2021-02-02 12:46:29 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Allow connection to global state
+import { Provider } from 'react-redux';
+// Allow for creation of global state and use of thunk
+import { createStore, applyMiddleware } from 'redux';
+// Allow for async
+import thunk from 'redux-thunk';
+// Grab rootReducer
+import rootReducer from './reducers/index';
+// Allow DevTools to be passed in with thunk
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store=createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import playbillReducer from './playbillReducer';
+
+const rootReducer = combineReducers({
+    playbillReducer
+});
+
+export default rootReducer;
