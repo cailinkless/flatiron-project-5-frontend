2bc99d3bc782671350e467b2223697825b6148ad
cailinkless <cailin.nysff@gmail.com>
2021-02-07 17:35:37 -0500

    adjust template to only included applicable sections

--- src/components/PlaybillTemplate.js
+++ src/components/PlaybillTemplate.js
@@ -18,48 +18,41 @@ state = {
 
 render() {
 
+  const titleLine = this.props.playbill.title !== "" ? <h2>{this.props.playbill.title}</h2> : <h2>Untitled Play</h2>
+  const playwrightLine = this.state.playwright ? <h3>Written by {this.state.playwright.name}</h3> : null
+  const directorLine = this.state.director ? <h3>Directed by {this.state.director.name}</h3> : null
+  const aboutSection = this.props.playbill.about !== "" ? <div className="about-wrapper"><h3>About the Show</h3><p>{this.props.playbill.about}</p></div> : null
+  const crewSection = this.state.crewmembers.length >= 1 ? <div className="crew-wrapper"><h3>Production Team</h3><ul>{this.state.crewmembers.map(cred => <li key={cred.id}>{cred.role}: {cred.name}</li>)}</ul></div> : null
+  const castSection = this.state.castmembers.length >= 1 ? <div className="cast-wrapper"><h3>Cast</h3><ul>{this.state.castmembers.map(cred => <li key={cred.id}>{cred.name} as {cred.role}</li>)}</ul></div> : null
+  const bioSection = this.state.allbios !== [] ? <div className="bios-wrapper"><h3>Bios:</h3>{this.state.allbios}</div> : null 
+
   return (
     <div>
 
       <div className="info-wrapper">
         
-        <h2>{this.props.playbill.title}</h2>
-        <h3>Written by {this.state.playwright.name}</h3>
-        <h3>Directed by {this.state.director.name}</h3>
+        {titleLine}
+        {playwrightLine}
+        {directorLine}
 
-        <hr/>
-
-        <h3>About the Show:</h3>
-        <p>{this.props.playbill.about}</p>
+        <hr/>   
 
       </div>
+
+      {aboutSection}
 
       <hr/>
 
       <div className="credits-wrapper">
 
-        <h3>Production Team:</h3>
-          <ul>
-            {this.state.crewmembers.map(cred => <li key={cred.id}>{cred.role}: {cred.name}</li>)}
-          </ul>
-
-        <h3>Cast:</h3>
-          <ul>
-              {this.state.castmembers.map(cred => <li key={cred.id}>{cred.name} as {cred.role}</li>)}
-          </ul>
+        {crewSection}
+        {castSection}
 
       </div>
 
       <hr/>
 
-      <div className="bios-wrapper">
-
-        <h3>Bios:</h3>
-
-        {this.state.allbios}
-        
-
-      </div>
+      {bioSection}
 
     </div>
   )}
